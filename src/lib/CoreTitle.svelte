6a8bd384501ede71905d6dbf6274b389e448330f
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2022-10-24 08:17:30 +0100

    Site Redesign

---
+++ src/lib/CoreTitle.svelte
@@ -0,0 +1,57 @@
+<!-- src/lib/CoreTitle.svelte -->
+<script>
+</script>
+
+<div>
+	<h1>Archie<br aria-hidden="true" />Biddiscombe<span class="blink" aria-hidden="true"> _</span></h1>
+</div>
+
+<style>
+	div {
+		margin: 0;
+		padding: 0;
+
+		display: inline-block;
+	}
+
+	div > h1 {
+		margin: 0;
+		padding: 0;
+
+		text-transform: lowercase;
+
+		font-size: 2.8em;
+		font-weight: 800;
+		font-family: -apple-system, BlinkMacSystemFont, avenir next, avenir,
+			helvetica neue, helvetica, sans-serif;
+
+		line-height: 0.9;
+
+		background: #fc5c7d;
+		background: linear-gradient(to right, #6a82fb, #fc5c7d);
+		-webkit-background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
+
+	div > h1 > span.blink {
+		animation: blink-animation 0.8s steps(5, start) infinite;
+		-webkit-animation: blink-animation 0.8s steps(5, start) infinite;
+		-moz-user-select: -moz-none;
+		-khtml-user-select: none;
+		-webkit-user-select: none;
+		-ms-user-select: none;
+		user-select: none;
+	}
+
+	@keyframes blink-animation {
+		to {
+			visibility: hidden;
+		}
+	}
+
+	@-webkit-keyframes blink-animation {
+		to {
+			visibility: hidden;
+		}
+	}
+</style>
---
+++ src/lib/SocialButtons.svelte
@@ -0,0 +1,62 @@
+<!-- src/lib/SocialButtons.svelte -->
+
+<script>
+
+	function redirectPage(url) {
+		window.location.assign(url);
+	}
+
+</script>
+
+<nav>
+	<button on:click={() => redirectPage("https://github.com/abiddiscombe")}>
+		<i class="bi bi-github" />
+		GitHub
+	</button>
+
+	<button on:click={() => redirectPage("https://linkedin.com/in/archiebiddiscombe")}>
+		<i class="bi bi-linkedin" />
+		LinkedIn
+	</button>
+</nav>
+
+<style>
+
+	nav {
+		margin: 0;
+		margin-top: 18px;
+		padding: 0;
+	}
+
+	nav > button {
+		margin: 0;
+		margin-right: 3px;
+		margin-bottom: 3px;
+		padding: 6px 14px;
+
+		cursor: pointer;
+
+		font-weight: 300;
+		font-size: 0.9em;
+		font-family: var(--font-family);
+
+		color: var(--l0-fg);
+		background: none;
+		
+		border: 1px solid #c9c9c9;
+		border-radius: 4px;
+	}
+
+	nav > button:hover {
+		transition-duration: 200ms;
+		border: 1px solid #ffffff;
+	}
+
+	nav > button > i {
+		margin-right: 6px;
+	}
+
+</style>
+
+
+
